dac7188f44dbcf1bb6afc667f806010764b2b594
mathieu <mathieu.perron95@outlook.com>
2020-09-10 14:08:51 -0400

    good config

---
+++ .emacs.d/elpa/ob-typescript-20190910.946/ob-typescript-autoloads.el
@@ -0,0 +1,16 @@
+;;; ob-typescript-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("ob-typescript.el") (24410 20480 189453
+;;;;;;  600000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ob-typescript-autoloads.el ends here
---
+++ .emacs.d/elpa/shrink-path-20190208.1335/shrink-path-autoloads.el
@@ -0,0 +1,16 @@
+;;; shrink-path-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("shrink-path.el") (24397 17747 324487
+;;;;;;  0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; shrink-path-autoloads.el ends here
